019c8dbf71ed92ea2d8451c4d4b2ac155264cb6f
ChaosJD <32511119+ChaosJD@users.noreply.github.com>
2021-12-12 12:01:44 +0100

    add hashCode() method example

--- @OverreideEquals().java
+++ @OverreideEquals().java
@@ -1,28 +1,41 @@
-public boolean euqals( Object obj){
-  if (obj == null ){
+class Person{
+  
+
+  public boolean euqals( Object obj){
+    if (obj == null ){
+     return false;
+    }
+  
+   if (obj == this){
+      return true;
+    }
+  
+   //Teste ob gleicher Datentyp
+    if(!this.getClass().equals(obj.getClass())){
+     return false;
+    }
+  
+    //Nur falls equals() in Oberklasse überschrieben wurde
+    if(super.equals(obj) == false){
+      return false;
+    }
+  
+    //Vergleich der Datenelemente, abhängig von Anwendung
+    Person other = (Person) obj; /// Typecast!
+    if (this.name.equals(other.name) && this.age == other.age){
+     return true;
+    }
+  
     return false;
+  
   }
   
-  if (obj == this){
-    return true;
-  }
-  
-  //Teste ob gleicher Datentyp
-  if(!this.getClass().equals(obj.getClass())){
-    return false;
-  }
-  
-  //Nur falls equals() in Oberklasse überschrieben wurde
-  if(super.equals(obj) == false){
-    return false;
-  }
-  
-  //Vergleich der Datenelemente, abhängig von Anwendung
-  Person other = (Person) obj; /// Typecast!
-  if (this.name.equals(other.name) && this.age == other.age){
-    return true;
-  }
-  
-  return false;
+ public int hashCode(){
+   int result = name.hashCode(); // all attributes
+   resultt = 31 * result + ageM; // Primzahl, *31  schell zu implementieren.
+   return result;
+ }
   
 }
+
+  
