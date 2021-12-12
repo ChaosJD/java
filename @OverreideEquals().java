0e097a32b4312eb17eef68f7446ecaeedb330498
ChaosJD <32511119+ChaosJD@users.noreply.github.com>
2021-12-12 11:49:04 +0100

    Create @OverreideEquals().java

---
+++ @OverreideEquals().java
@@ -0,0 +1,28 @@
+public boolean euqals( Object obj){
+  if (obj == null ){
+    return false;
+  }
+  
+  if (obj == this){
+    return true;
+  }
+  
+  //Teste ob gleicher Datentyp
+  if(!this.getClass().equals(obj.getClass())){
+    return false;
+  }
+  
+  //Nur falls equals() in Oberklasse überschrieben wurde
+  if(super.equals(obj) == false){
+    return false;
+  }
+  
+  //Vergleich der Datenelemente, abhängig von Anwendung
+  Person other = (Person) obj; /// Typecast!
+  if (this.name.equals(other.name) && this.age == other.age){
+    return true;
+  }
+  
+  return false;
+  
+}
